cc4d1bc01065856c725a01d4b9f866bf68792099
Audrey-MENAESSE <61827889+Audrey-MENAESSE@users.noreply.github.com>
2020-05-17 21:19:05 +0200

    3D Gaussian added

--- Morpho_3D.java
+++ Morpho_3D.java
@@ -1,10 +1,9 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+
 
 import ij.IJ;
 import ij.ImagePlus;
 import ij.gui.GenericDialog;
-import ij.measure.ResultsTable;
+
 import ij.plugin.PlugIn;
 import ij.process.ImageProcessor;
 
@@ -14,36 +13,73 @@ public class Morpho_3D implements PlugIn {
 	public void run(String arg) {
 		
 		ImagePlus in = IJ.getImage().duplicate();
-		GenericDialog dlg = new GenericDialog("Morphological Operators Parameters");
-		dlg.addNumericField("Size of 3D circular structuring element (odd)", 3, 1);
-		dlg.showDialog();
-		if (dlg.wasCanceled()) return;
-		int size = (int) dlg.getNextNumber();
-		boolean[][][] b = ball(size);
-		ImagePlus out = close (in, b);
+		
+		ImagePlus out = gaussian3D (in, 1.5, 1.5);
+		
+		
+		//GenericDialog dlg = new GenericDialog("Morphological Operators Parameters");
+		//dlg.addNumericField("Size of 3D circular structuring element (odd)", 3, 1);
+		//dlg.showDialog();
+		//if (dlg.wasCanceled()) return;
+		//int size = (int) dlg.getNextNumber();
+		//boolean[][][] b = ball(size);
+		//ImagePlus out = close (in, b);
 		out.show();
 	
 	}
 	
-	public double[][][] neighborhood (ImagePlus in, int x, int y, int z, int t, int L){
-		double [][][] b = new double [L][L][L];
+	public double[][][] neighborhood (ImagePlus in, int x, int y, int z, int t, int Lxy, int Lz){
+		double [][][] b = new double [Lxy][Lxy][Lz];
 		//Arrays.fill(b, 0);
-		int l= (L-1)/2;
+		int lxy= (Lxy-1)/2;
+		int lz= (Lz-1)/2;
 		
-		if (z>l && z<(in.getNSlices()-l)) {
-		if (x>l && x<(in.getWidth()-l) && y>l && y<(in.getHeight()-l)) {
-			for (int i=0; i<L; i++) {
-				for (int j=0; j<L; j++) {
-					for (int k=0; k<L; k++) {
-						in.setPosition(1, z-l+k, t);
+		if (z>lz && z<(in.getNSlices()-lz)) {
+		if (x>lxy && x<(in.getWidth()-lxy) && y>lxy && y<(in.getHeight()-lxy)) {
+			for (int i=0; i<Lxy; i++) {
+				for (int j=0; j<Lxy; j++) {
+					for (int k=0; k<Lz; k++) {
+						in.setPosition(1, z-lz+k, t);
 						ImageProcessor ip = in.getProcessor();
-						b[i][j][k] = ip.getPixelValue(x-l+i, y-l+j);
+						b[i][j][k] = ip.getPixelValue(x-lxy+i, y-lxy+j);
 					}
 					
 				}
 			}
 		}
 		}
+		if (z<=lz) {
+			if (x>lxy && x<(in.getWidth()-lxy) && y>lxy && y<(in.getHeight()-lxy)) {
+				for (int i=0; i<Lxy; i++) {
+					for (int j=0; j<Lxy; j++) {
+						for (int k=0; k<Lz; k++) {
+							while(z-lz+k<0) {
+								b[i][j][k] = 0;
+							}
+							in.setPosition(1, z-lz+k, t);
+							ImageProcessor ip = in.getProcessor();
+							b[i][j][k] = ip.getPixelValue(x-lxy+i, y-lxy+j);
+						}
+					}}
+			}
+			
+			if (z>=(in.getNSlices()-lz)) {
+				if (x>lxy && x<(in.getWidth()-lxy) && y>lxy && y<(in.getHeight()-lxy)) {
+					for (int i=0; i<Lxy; i++) {
+						for (int j=0; j<Lxy; j++) {
+							for (int k=0; k<Lz; k++) {
+								while(z-lz+k<=in.getNSlices()) {
+									in.setPosition(1, z-lz+k, t);
+									ImageProcessor ip = in.getProcessor();
+									b[i][j][k] = ip.getPixelValue(x-lxy+i, y-lxy+j);
+								}
+								b[i][j][k]=0;
+							}
+						}}
+				}
+			
+		}}
+		
 		
 		return b;
 	}
@@ -81,7 +117,8 @@ public class Morpho_3D implements PlugIn {
 			 for (int x=0; x<nx; x++) {
 				 for (int y=0; y<ny; y++) {
 					 for (int z=0; z<nz; z++) {
-						 double [][][] block = neighborhood (in, x, y, z, t, size);
+						 IJ.log(""+z);
+						 double [][][] block = neighborhood (in, x, y, z, t, size, size);
 						 double max = -Double.MAX_VALUE;
 						 for (int i=0; i<size; i++) {
 							 for (int j=0; j<size; j++) {
@@ -114,7 +151,7 @@ public class Morpho_3D implements PlugIn {
 			 for (int x=0; x<nx; x++) {
 				 for (int y=0; y<ny; y++) {
 					 for (int z=0; z<nz; z++) {
-						 double [][][] block = neighborhood (in, x, y, z, t, size);
+						 double [][][] block = neighborhood (in, x, y, z, t, size, size);
 						 double min = Double.MAX_VALUE;
 						 for (int i=0; i<size; i++) {
 							 for (int j=0; j<size; j++) {
@@ -139,6 +176,47 @@ public class Morpho_3D implements PlugIn {
 		 return erosion(dilation(in,mask),mask);
 		 }
 
+	public ImagePlus gaussian3D (ImagePlus in, double sigmaxy, double sigmaz) {
+		int nx = in.getWidth();
+		 int ny = in.getHeight();
+		 int nt = in.getNFrames();
+		 int nz = in.getNSlices();
+		 int bitdepth = in.getBitDepth();
+		 ImagePlus output = IJ.createHyperStack("Out", nx, ny, 1, nz, nt, bitdepth);
+		 int lxy = (int) (2 * Math.ceil(3 * sigmaxy) + 1);
+		 int lz = (int) (2*Math.ceil(3*sigmaz)+1);
+		 		 
+		 double[][][] g3D = new double [lxy][lxy][lz];
+		 for (int x=0; x<lxy; x++) {
+			 for(int y=0; y<lxy;y++) {
+				 for (int z=0; z<lz; z++) {			 
+				 g3D[x][y][z] = Math.exp(-(Math.pow(x-(lxy-1)/2,2)+Math.pow(y-(lxy-1)/2,2))/(2*sigmaxy*sigmaxy)-Math.pow(z-(lz-1)/2,2)/(2*sigmaz*sigmaz))/(2*Math.PI*sigmaxy*sigmaxy*sigmaz*Math.sqrt(2*Math.PI));
+				 }}}
+		 for (int t=0; t<nt; t++) {
+			 for(int x=0; x<nx; x++) {
+				 for(int y=0; y<ny; y++) {
+					 for (int z=0; z<nz; z++){
+						 double[][][] b = neighborhood(in, x, y, z, t, lxy, lz);
+						 double pixel = 0.0;
+			 			 for (int i=0; i<lxy; i++) {
+			 				 for (int j=0; j<lxy; j++) {
+			 					 for (int k=0; k<lz; k++) {
+			 						pixel=pixel + g3D[i][j][k]*b[i][j][k];
+			 					 }
+			 				 }
+			 			 }
+			 			 output.setPosition(1,z,t);
+						 ImageProcessor ip = output.getProcessor();
+						 ip.putPixelValue(x,y,pixel);	
+					 }}
+			 			 
+			 
+			 }
+			 }
+
+		 
+		 return output;
+	}
 		 
 
 }
