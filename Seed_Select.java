43004f0492ff66b9165bdd78f6fab05932baa167
Audrey-MENAESSE <61827889+Audrey-MENAESSE@users.noreply.github.com>
2020-05-31 11:37:29 +0200

    Add files via upload

--- Seed_Select.java
+++ Seed_Select.java
@@ -3,6 +3,8 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
 
+import com.sun.tools.javac.util.ArrayUtils;
+
 
 import ij.IJ;
 import ij.ImagePlus;
@@ -29,26 +31,16 @@ public class Seed_Select implements PlugIn {
 		int nz = in.getNSlices();
 		int b = in.getBitDepth();
 		
+		// Opening the edges image
 		OpenDialog od = new OpenDialog("Choose a .tif file", null);  
 		String path = od.getPath();
 		IJ.open(path);
 		ImagePlus edges = IJ.getImage();
-		
-		
-		/*
-		// Opening the edges file
-		String path = getPath(arg);  
-        if (null == path) return; 
-        ImagePlus edges = get_open(path, nx, ny, nz, nt);
-        edges.show();
-		*/
-		
-		
-		
-				
+			
+		// Checking that the seeds are selected from the last time point		
 		int slice = in.getSlice();
 		int frame = in.getFrame();
-		if (frame < nt-1) {
+		if (frame < nt) {
 			IJ.log("The initial seeds must be selected from the last frame");
 			return;
 		}
@@ -67,17 +59,18 @@ public class Seed_Select implements PlugIn {
 		int nri = up.xpoints.length;
 		ArrayList<Point3D> seeds = new ArrayList<Point3D>();
 		
+		// Creating the list of seed points for the last frame
 		ImageProcessor imp = in.getProcessor();
 		for (int i = 0; i < nri; i++){			
 			Point3D seed = new Point3D(up.xpoints[i], up.ypoints[i], slice, frame, imp.getPixelValue(up.xpoints[i], up.ypoints[i]));
 			seeds.add(seed);
 			IJ.log("Seed added: " + seed.toString() + "for frame "+ nt);
-		}
-		
+		}		
 		IJ.log(seeds.size()+" seeds for frame " + (nt));
-		// Looping over the frames
+		
+		// Recursively defining the list of seed points for each frame
 		for (int i=1; i<nt; i++) {			
-			seeds = getSeed (edges, in, seeds, slice, nt-i);
+			seeds = getSeeds (edges, in, seeds, slice, nt-i);
 			IJ.log(seeds.size()+" seeds for frame " + (nt-i));
 		}
 		
@@ -85,149 +78,104 @@ public class Seed_Select implements PlugIn {
 	
 	
 	
-	
-	
-	
-	public ArrayList<Point3D> getSeed (ImagePlus edges, ImagePlus in, ArrayList<Point3D> seeds, int slice, int frame) { 
+	public ArrayList<Point3D> getSeeds (ImagePlus edges, ImagePlus in, ArrayList<Point3D> seeds, int slice, int frame) { 
 		in.setPosition(1, slice, frame);
 		ImageProcessor ip = in.getProcessor();
 		edges.setPosition(1,slice, frame);
 		ImageProcessor ipedges = edges.getProcessor();
-		ArrayList<Point3D> newSeeds = new ArrayList<Point3D>();
-				
+		ArrayList<Point3D> newSeeds =  (ArrayList<Point3D>) seeds.clone();
+		
 		int nri = seeds.size();
-		while(nri>0) {
-			Point3D A = seeds.get(0);
-			int separation = 0; 
-						
-				for (int j=1; j<nri; j++) {
-					Point3D B = seeds.get(j);
-					if (isSeparated(ipedges,A,B)==false) {
-						separation =j;											
-					}
-				}
-				if(separation ==0) {
-					Point3D C = new Point3D (A.getX(), A.getY(), slice, frame, ip.getPixelValue(A.getX(), A.getY()));
-					newSeeds.add(C);
-					seeds.remove(0);
-					//IJ.log("Seed updated : " + C.getX() + "  "  + C.getY() + " for frame " + frame);				
-				}
-				else {
-					Point3D B = seeds.get(separation);
-					int x = (int) Math.floor((A.getX() + B.getX())/2);
-					int y = (int) Math.floor((A.getY() + B.getY())/2);
-					Point3D C = new Point3D (x, y, slice, frame, ip.getPixelValue(x, y));
-					newSeeds.add(C);
-					seeds.remove(separation);
-					seeds.remove(0);
-					//IJ.log("Seed created : " + C.getX() + "  "  + C.getY() + " for frame " + frame);	
-				}
-				nri = seeds.size();
+		ArrayList<Integer> discarded = new ArrayList<Integer>();
+		for (int i=0; i<nri; i++) {
+			if (discarded.contains(i)==false) {
+			// Checking if the seed is separated from the other seed points by a membrane on the edges file. 
+			int loc = isAlone(ipedges, seeds, discarded, i);
+			// If the seed is independent, the value of the associated pixel is update to the current time frame
+			if (loc==Integer.MAX_VALUE) {
+				Point3D A = seeds.get(i);
+				Point3D C = new Point3D (A.getX(), A.getY(), slice, frame, ip.getPixelValue(A.getX(), A.getY()));
+				newSeeds.set(i, C);
+				IJ.log("Seed updated : " + C.getX() + "  "  + C.getY() + " for frame " + frame);		
 			}
-	
-			
-			
-		return newSeeds;
+			// When 2 seed points are in the same region, they are replaced by a seed point situated in the middle of the segment defined by these two points 
+			else {
+				Point3D A = seeds.get(i);
+				Point3D B = seeds.get(loc);
+				int x = (int) Math.floor((A.getX() + B.getX())/2);
+				int y = (int) Math.floor((A.getY() + B.getY())/2);
+				Point3D C = new Point3D (x, y, slice, frame, ip.getPixelValue(x, y));
+				discarded.add(i);
+				discarded.add(loc);
+				newSeeds.add(C);
+				IJ.log("Seed created : " + C.getX() + "  "  + C.getY() + " for frame " + frame);
+				IJ.log("2 seeds removed");
+			}
+			}
+		}
+		// Creating a new list to only keep the isolated seeds 
+		int s= discarded.size();
+		ArrayList<Point3D> NS =  new ArrayList<Point3D>();
+		for (int k=0; k<newSeeds.size(); k++) {
+			if (discarded.contains(k)==false) {
+				Point3D A = newSeeds.get(k);
+				NS.add(A);
+			}
+		}
+		
+		return NS;
 		
 	}
 	
-	
-	public boolean isSeparated (ImageProcessor ipedges, Point3D A, Point3D B) {
-		int xmin = Math.min(A.getX(), B.getX());
-		int xmax;
-		int ymin;
-		int ymax;
-		if (xmin==A.getX()) {
-			xmax = B.getX();
-			ymin = A.getY();
-			ymax = B.getY();
-		}
-		else {
-			xmax = A.getX();
-			ymin = B.getY();
-			ymax = A.getY();
-		}
-		double slope=0;
-		if (xmax!=xmin) {
-		slope = (ymax-ymin)/(xmax-xmin);
-		}
+	public int isAlone (ImageProcessor ipedges, ArrayList<Point3D> seeds, ArrayList<Integer> removed, int i) {
+		int nri = seeds.size();
+		int loc = Integer.MAX_VALUE;
+		Point3D A = seeds.get(i);
+		for (int k=0; k<nri; k++) {
+			//Checking if the point has already been removed 
+			if (removed.contains(k)==false && k!=i) {
+				Point3D B = seeds.get(k);
+				
+				int dx = Math.abs(A.getX()-B.getX());
+				int dy = Math.abs(A.getY()-B.getY());
+				int lth = (int) Math.sqrt(dx*dx+dy*dy);
 		
-		double intercept = ymax - slope*xmax;
-		double intensity = 0;
-		for (int i=0; i <(xmax-xmin); i++) {
-			int x = xmin + i ;
-			int y = (int) ((int) slope*x + intercept);
-			intensity = intensity + ipedges.getPixelValue(x, y);
-		}
+				double intensity = 0;
+		int x=Math.min(A.getX(), B.getX());
+		int y=Math.min(A.getY(), B.getY());
 		
-		if (intensity ==0) {
-			return false;
-		}
-		else {
-			return true;
+		// Detecting membrane along the line between the 2 points
+		for (int n=0; n <lth; n++) {
+			
+			if (dx!=0) {
+			x = x + 1/dx ;
+			}
+			if(dy!=0) {
+			y = y + 1/dy;
+			}
+			intensity+= ipedges.getPixelValue(x, y);
+			intensity+= ipedges.getPixelValue(x, y-1);
+			intensity+= ipedges.getPixelValue(x, y+1);
+			intensity+= ipedges.getPixelValue(x-1, y);
+			intensity+= ipedges.getPixelValue(x-1, y-1);
+			intensity+= ipedges.getPixelValue(x-1, y+1);
+			intensity+= ipedges.getPixelValue(x+1, y);
+			intensity+= ipedges.getPixelValue(x+1, y-1);
+			intensity+= ipedges.getPixelValue(x+1, y+1);
+			}
+		
+			if (intensity != 0) {
+				loc=k;
+			}
+			
 		}
 		}
 	
+	return loc; 
+	
+}
 }
 	
 	
-	/* Garbage Code to open the second image
-	
-	public String getPath(String arg) {  
-        OpenDialog od = new OpenDialog("Choose a .tif file", null);  
-        String dir = od.getDirectory();  
-        if (null == dir) return null; // dialog was canceled  
-        dir = dir.replace('\\', '/'); // Windows safe  
-        if (!dir.endsWith("/")) dir += "/";  
-        return dir + od.getFileName();  
-    }  
-	
-	public InputStream open(String path) throws Exception {  
-	       return new FileInputStream(path);  
-	    }  
-	
-	 public ImagePlus get_open(String path, int nx, int ny, int nz, int nt) {  
-	            
-	         
-	        // Build a new FileInfo object with all file format parameters and file data  
-	        FileInfo fi = new FileInfo();  
-	        fi.fileFormat = fi.RAW;  
-	        int islash = path.lastIndexOf('/');  
-	        if (0 == path.indexOf("http://")) {  
-	            fi.url = path;  
-	        } else {  
-	            fi.directory = path.substring(0, islash+1);  
-	        }  
-	        fi.fileName = path.substring(islash+1);  
-	        fi.width = nx;  
-	        fi.height = ny;  
-	        fi.nImages = nz*nt;
-	        fi.gapBetweenImages = 0;  
-	        fi.intelByteOrder = true; // little endian  
-	        fi.whiteIsZero = false; // no inverted LUT  
-	        //fi.longOffset = fi.offset = 512; // header size, in bytes  
-	  
-	        // Now make a new ImagePlus out of the FileInfo  
-	        // and integrate its data into this PlugIn, which is also an ImagePlus  
-	         
-	            FileOpener fo = new FileOpener(fi);  
-	            ImagePlus imp = fo.open(false);  
-	            imp.setStack(imp.getTitle(), imp.getStack());  
-	            imp.setCalibration(imp.getCalibration());  
-	            Object obinfo = imp.getProperty("Info");  
-	            if (null != obinfo) imp.setProperty("Info", obinfo);  
-	            imp.setFileInfo(imp.getOriginalFileInfo());    
-	        return imp ; 
-	    }
-	    
-	    public final int readIntLittleEndian(byte[] buf, int start) {  
-	        return (buf[start]) + (buf[start+1]<<8) + (buf[start+2]<<16) + (buf[start+3]<<24);  
-	    }  
-	    
 	
 	
-
-	}
-
-*/
-
