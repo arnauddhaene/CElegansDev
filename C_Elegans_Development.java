665fdda6a5dbd5d50995db8fae20d5536c6f4163
Audrey-MENAESSE <61827889+Audrey-MENAESSE@users.noreply.github.com>
2020-06-02 18:20:32 +0200

    Add files via upload

--- C_Elegans_Development.java
+++ C_Elegans_Development.java
@@ -78,7 +78,7 @@ public class C_Elegans_Development implements PlugIn {
 		
 		// Create output segmentation image
 		ImagePlus areas = IJ.createHyperStack("Regions", nx, ny, 1, nz, nt, b);
-		areas.show();
+		//areas.show();
 		
 		// Checking that the seeds are selected from the last time point		
 		int slice = in.getSlice();
@@ -260,13 +260,15 @@ public class C_Elegans_Development implements PlugIn {
 			iter++;
 			
 		}
+		IJ.log("Closing");
 		areas = close (areas, frame, ball(3));
 		seeds = repositionSeeds (regions, in, seeds, slice, frame);
 		
 		
 		}
 		
-		
+		IJ.saveAs(areas, "Tiff", dir +"/areas.tif");
+		areas.show();
 		// End of region growing
 		
 		
